ee07e34c646a927df71b37b054e7b237357b54bc
TraNNguyenQuocThai <thaimini27092004.@gmail.com>
2026-03-11 09:35:55 +0700

    Commit 4 on A: Add object properties to file4.js

--- file4.js
+++ file4.js
@@ -1,1 +1,6 @@
 console.log("Hello from file4.js");
+// Commit 4 on A
+let person = { firstName: "John", lastName: "Doe" };
+person.fullName = person.firstName + " " + person.lastName;
+console.log("Person:", person);
+console.log("Full Name:", person.fullName);
