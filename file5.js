f325d741108995462430a0de741e1e9c84c2dd0b
TraNNguyenQuocThai <thaimini27092004.@gmail.com>
2026-03-11 09:37:03 +0700

    Commit 5 on A: Add string manipulation to file5.js

--- file5.js
+++ file5.js
@@ -1,1 +1,7 @@
 console.log("Hello from file5.js");
+// Commit 5 on A
+let sentence = "This is a test sentence.";
+let words = sentence.split(" ");
+console.log("Words:", words);
+let reversed = words.reverse().join(" ");
+console.log("Reversed:", reversed);
