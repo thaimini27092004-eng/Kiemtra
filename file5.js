dec1e41419d5f8cfd8470e77f974d5f893d117af
TraNNguyenQuocThai <thaimini27092004.@gmail.com>
2026-03-11 09:34:50 +0700

    Commit 5 on B: Add string operations to file5.js

--- file5.js
+++ file5.js
@@ -1,1 +1,7 @@
 console.log("Hello from file5.js");
+// Commit 5 on B
+let str = "Hello World";
+let upper = str.toUpperCase();
+let lower = str.toLowerCase();
+console.log("Upper:", upper);
+console.log("Lower:", lower);
