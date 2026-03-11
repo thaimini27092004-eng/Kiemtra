dcdbdcc0175d2b621b9a56ad25ee6090226f2c49
TraNNguyenQuocThai <thaimini27092004.@gmail.com>
2026-03-11 09:35:44 +0700

    Commit 3 on A: Add array operations to file3.js

--- file3.js
+++ file3.js
@@ -1,1 +1,7 @@
 console.log("Hello from file3.js");
+// Commit 3 on A
+let fruits = ["apple", "banana", "cherry"];
+fruits.push("date");
+console.log("Fruits:", fruits);
+let length = fruits.length;
+console.log("Length:", length);
