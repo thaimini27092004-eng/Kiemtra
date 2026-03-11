80334f10e89803dc41ceb6923cb1f2d433553e11
TraNNguyenQuocThai <thaimini27092004.@gmail.com>
2026-03-11 09:35:33 +0700

    Commit 2 on A: Add area calculation to file2.js

--- file2.js
+++ file2.js
@@ -1,1 +1,9 @@
 console.log("Hello from file2.js");
+// Commit 2 on A
+function calculateArea(width, height) {
+    return width * height;
+}
+let area = calculateArea(4, 5);
+console.log("Area:", area);
+let perimeter = 2 * (4 + 5);
+console.log("Perimeter:", perimeter);
