c203f7901b3dced251bddd4be7febdc84ae0f67f
TraNNguyenQuocThai <thaimini27092004.@gmail.com>
2026-03-11 09:35:16 +0700

    Commit 1 on A: Add arithmetic operations to file1.js

--- file1.js
+++ file1.js
@@ -1,1 +1,8 @@
 console.log("Hello from file1.js");
+// Commit 1 on A: Addition
+let p = 5;
+let q = 10;
+let total = p + q;
+console.log("Total:", total);
+let diff = q - p;
+console.log("Difference:", diff);
